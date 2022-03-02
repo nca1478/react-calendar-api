4a45f5a82aa2ec5cc3b321191bf217cc41a4173b
Nelson Cadenas <nca1478@yahoo.com>
2022-03-01 21:05:58 -0400

    added: express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,36 @@
 const { request, response } = require('express')
+const { validationResult } = require('express-validator')
 
 const createUser = (req = request, res = response) => {
-  const { name, email, password } = req.body
+  const errors = validationResult(req)
 
-  if (name.length < 5) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: 'Name must be at least 5 characters',
+      errors: errors.mapped(),
     })
   }
 
   res.status(201).json({
     ok: true,
-    msg: 'User created',
+    msg: 'User created successfully',
     user: req.body,
   })
 }
 
 const loginUser = (req = request, res = response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    })
+  }
+
   res.json({
     ok: true,
-    msg: 'User Login',
+    msg: 'Login succesfully',
     user: req.body,
   })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 const { Router } = require('express')
 const { createUser, loginUser, renewToken } = require('../controllers/auth')
+const { check } = require('express-validator')
 const router = Router()
 
-router.post('/new', createUser)
-router.post('/', loginUser)
+router.post(
+  '/new',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is not correct').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  createUser
+)
+router.post(
+  '/',
+  [
+    check('email', 'Email is not correct').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  loginUser
+)
 router.get('/renew', renewToken)
 
 module.exports = router
