b750652c45cdd88e0d45829b369b72d2a39ee34a
Nelson Cadenas <nca1478@yahoo.com>
2024-08-18 12:49:00 -0400

    added: api docker files (docker folder)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// DB Connection
+dbConnection();
+
+// Middlewares
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en puerto: ${PORT}`);
+});
